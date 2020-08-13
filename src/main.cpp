8a4813d0ece2876ac27890dbe30162eace3b72e7
ImagineTitanz <rriverab1800@alumno.ipn.mx>
2020-08-13 16:28:22 -0500

    Cambios simples

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   pinMode(1, OUTPUT);
   digitalWrite(1, LOW);
 }
 
-void loop() {
-  digitalWrite(1, HIGH);
-  delay(1000);
+void loop()
+{
   digitalWrite(1, LOW);
   delay(1000); // delay de 1 segundo
+  digitalWrite(1, HIGH);
+  delay(1000);
 }
