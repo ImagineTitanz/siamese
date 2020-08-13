13a8dc93a84221991f42fc90ad378b540f03f022
ImagineTitanz <rriverab1800@alumno.ipn.mx>
2020-08-13 16:57:20 -0500

    Agregamos librería Firebaseesp8266 y sus comandos principales

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
+
 #include <FirebaseESP8266.h>
+#define FIREBASE_HOST "YOUR_FIREBASE_PROJECT.firebaseio.com"
+#define FIREBASE_AUTH "YOUR_FIREBASE_DATABASE_SECRET"
+#define WIFI_SSID "YOUR_WIFI_AP"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+FirebaseData firebaseData;
+String path = "/cmss/sensors"; //Se declara la rama de la base de datos a utilizar
 
 void setup()
 {
-  pinMode(1, OUTPUT);
-  digitalWrite(1, LOW);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //Se inicializa la conexiòn con la red WiFi
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); //Se inicializa la comunicaciòn con la base de datos
+  Firebase.reconnectWiFi(true); //
+
+  Firebase.enableClassicRequest(firebaseData, true); //Se permite el GET y POST de los datos en redes con Firewall
+
+  pinMode(1, OUTPUT); //Se declara el pin 5 de Tx (GPIO-1) como salida
+  digitalWrite(1, LOW); // Se sobreescribe el valor de GPIO-1 a 0
 }
 
 void loop()
